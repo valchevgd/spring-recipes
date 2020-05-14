59b389ad6e6ca597c697c5290ec621e1dd0d5cb4
valchevgd <valchevgd@gmail.com>
2020-05-14 23:11:04 +0300

    HomeController and index page added!

---
+++ src/main/java/com/github/valchevgd/springrecipes/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.github.valchevgd.springrecipes.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping({ "", "/", "index" })
+    public String getIndexPage() {
+        return "index";
+    }
+}
