5cc03bde26ad2e1152619237d3a34b77026c54c7
valchevgd <valchevgd@gmail.com>
2020-05-14 22:50:43 +0300

    Initial commit

---
+++ src/main/java/com/github/valchevgd/springrecipes/SpringRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.github.valchevgd.springrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipesApplication.class, args);
+    }
+
+}
